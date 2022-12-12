f7583708d2cc0b208db08b1bf85dbd111017d99f
ukro11 <41793256+ukro11@users.noreply.github.com>
2022-12-12 18:18:10 +0100

    website

--- code.js
+++ code.js
@@ -16,22 +16,11 @@ class Product {
     }
 }
 
-function addProduct(pr) {
-    const pr_html = `<div class="s2_pr"><img src="./img/${pr.image}"><div class="s2_pr_desc"><h3>${pr.company}</h3><h4>${pr.title}</h4><h5>${pr.description}</h5><h5><b>Ab</b>${pr.price}</h5></div></div>`;
-    s2_product_container.innerHTML += pr_html;
-}
-
-function addProductToCart(pr) {
-    const pr_html = `<div class="s1_checkout_item"><div><img src="./img/${pr.image}"></div><div><h3>${pr.title}</h3><h4>${pr.price}</h4></div><div><ion-icon name="close"></ion-icon></div></div>`
-    const cart_items = document.querySelector('.s1_checkout_items');
-    cart_items.innerHTML += pr_html;
-}
-
 const registered_products = [
     // iPhone 11
     new Product('iPhone 11', 'Apple', 'Refurbished iPhone 11 128 GB - Violett (ohne Vertrag)', '499,99€', 'iphone_11.png'),
     new Product('iPhone 11 Pro', 'Apple', 'Refurbished iPhone 11 Pro 64 GB - Space Grau (ohne Vertrag)', '699,99€', 'iphone_11_pro.png'),
-    new Product('iPhone 11 Pro Max', 'Apple', 'Refurbished iPhone 11 Pro Max 64 GB - Silber (ohne Vertrag)', '859,00 €', 'iphone_11_pro_max.png'),
+    new Product('iPhone 11 Pro Max', 'Apple', 'Refurbished iPhone 11 Pro Max 64 GB - Silber (ohne Vertrag)', '859,00€', 'iphone_11_pro_max.png'),
     // iPhone 12
     new Product('iPhone 12 mini', 'Apple', 'Refurbished iPhone 12 mini 128 GB - Violett (ohne Vertrag)', '599,99€', 'iphone_12_mini.png'),
     new Product('iPhone 12', 'Apple', 'Refurbished iPhone 12 128 GB - Schwarz (ohne Vertrag)', '699,99€', 'iphone_12.png'),
@@ -39,14 +28,43 @@ const registered_products = [
     new Product('iPhone 12 Pro Max', 'Apple', 'Refurbished iPhone 12 Pro Max 128 GB - Pazifikblau (ohne Vertrag)', '999,99€', 'iphone_12_pro_max.png')
 ];
 
+const productsInCart = [];
+
+function addProduct(pr) {
+    const pr_html = `<div class="s2_pr"><img src="./img/${pr.image}"><div class="s2_pr_desc"><h3>${pr.company}</h3><h4>${pr.title}</h4><h5>${pr.description}</h5><h5>Ab <a>${pr.price}</a></h5></div></div>`;
+    s2_product_container.innerHTML += pr_html;
+}
+
+function addProductToCart(pr, addedByStorage) {
+    const pr_html = `<div class="s1_checkout_item"><div><img src="./img/${pr.image}"></div><div><h4>${pr.company}</h4><h3>${pr.title}</h3><h5>Artikel entfernen</h5></div><div><h5>${pr.price}</h5></div></div>`;
+    const cart_items = document.querySelector('.s1_checkout_items');
+    cart_items.innerHTML += pr_html;
+    productsInCart.push(pr);
+    if (!addedByStorage) {
+        var value = localStorage.getItem('productsInCart') === null || localStorage.getItem('productsInCart') === undefined ? `${pr.title}` : `${localStorage.getItem("productsInCart")};${pr.title}`;
+        localStorage.setItem('productsInCart', value);
+    }
+}
+
+//localStorage.clear()
+
+function getProductFromStorage(val) {
+    return registered_products.filter(pr => pr.title === val)[0];
+}
+
 registered_products.forEach(pr => addProduct(pr));
 
 const products = document.querySelectorAll('.s2_pr');
 products.forEach(pr => {
     pr.addEventListener('click', () => {
         const index = Array.from(products).indexOf(pr);
-        addProductToCart(registered_products[index])
+        addProductToCart(registered_products[index], false);
     });
 });
+
+if (localStorage.getItem('productsInCart') !== null) {
+    const saved_products = localStorage.getItem('productsInCart').split(';');
+    saved_products.forEach(pr => addProductToCart(getProductFromStorage(pr), true));
+}
 
 // TODO: Open/Close Cart, Calculate Price, Remove Product from Cart
